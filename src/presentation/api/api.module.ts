c61490ac04a860595ed0323b125277650c69f1d7
AntoineMezon <antoine.mezon@epitech.eu>
2024-03-20 04:06:18 +0100

    feat(module): imports new comments module

--- src/presentation/api/api.module.ts
+++ src/presentation/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ArticlesApiModule } from "./articles/articles.module";
 import { AuthApiModule } from "./auth/auth.module";
+import { CommentsApiModule } from "./comments/comments.module";
 
 @Module({
-    imports: [ArticlesApiModule, AuthApiModule],
+    imports: [ArticlesApiModule, AuthApiModule, CommentsApiModule],
 })
 export class ApiModule {}
